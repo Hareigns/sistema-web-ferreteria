09b9bed13ff3f0a355d8e998b620e175ade982f7
Melanie <melanievelasquezalvarado@gmail.com>
2025-03-16 00:07:27 -0600

    Añadir vistas

--- backend/src/keys.js
+++ backend/src/keys.js
@@ -1,4 +1,15 @@
- /*
+ export const database = {
+    host: 'localhost',
+    port: '3306',
+    user: 'user',
+    password: '26102004',
+    database: 'ferreteria'
+  };
+ 
+  
+  
+  
+   /*
  export const database = {
     host: 'localhost',
     port: '3306',
@@ -7,14 +18,4 @@
     database: 'ferreteria'
   };
   */
-  
-  
-  export const database = {
-    host: 'localhost',
-    port: '3306',
-    user: 'admin',
-    password: 'admin',
-    database: 'ferreteria'
-  };
-  
   
